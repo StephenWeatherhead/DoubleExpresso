c3637b3b264de0905093daf71c91dbf40ea62073
Stephen Weatherhead <stephenw42@outlook.com>
2023-01-07 17:13:13 +0000

    DoubleExpresso interface

---
+++ DoubleExpresso/IExpressoEvaluator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoubleExpresso
+{
+    public interface IExpressoEvaluator
+    {
+        double Evaluate(string p_ExpressionString, List<ExpressoVar> p_Variables, List<IExpressoFunc> p_Functions);
+    }
+    public class ExpressoVar
+    {
+        public string Name { get; set; }
+        public double Value { get; set; }
+    }
+    public interface IExpressoFunc
+    {
+        string Name { get; }
+        int NumberOfParams { get; }
+        double Execute(List<double> p_Parameters);
+    }
+}
